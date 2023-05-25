96ae4ad4e5bae00208ae2e20fb154f0869485b0c
Quế Lâm Vũ <lamcherry0208@gmail.com>
2023-05-25 14:55:48 +0700

    fix gencode

--- src/screen/HomePage/index.tsx
+++ src/screen/HomePage/index.tsx
@@ -4,7 +4,8 @@ import {View, TouchableOpacity, Text} from 'react-native';
 import { useNavigation} from '@react-navigation/native';
 import {ref, onValue, set} from 'firebase/database';
 import {database} from '../../configs/firebase/firebaseconfig';
-import QRCodeGenerator from "../QRCodeGenerator";
+import { updateById } from '../../utils/HandleFuntions';
+// import QRCodeGenerator from "../QRCodeGenerator";
 
 
 interface Idata {
@@ -16,31 +17,6 @@ interface Idata {
   Valid: boolean;
 }
 
-function updateById(docId: string, Object: Idata) {
-  const docRef = ref(
-    database,
-    '142Ei6ewo87GYD-f6qzL_V-KmKtBQN5s6cDObMkgsBXI/Sheet1/' + docId,
-  );
-  let doc: Idata;
-  onValue(docRef, snapshot => {
-    doc = snapshot.val();
-  });
-
-  set(
-    ref(
-      database,
-      '142Ei6ewo87GYD-f6qzL_V-KmKtBQN5s6cDObMkgsBXI/Sheet1/' + docId,
-    ),
-    {
-      ID: Object.ID,
-      Name: Object.Age,
-      Age: Object.Age,
-      Email: Object.Email,
-      LinkQR: '123566',
-      Valid: Object.Valid,
-    },
-  );
-}
 
 export default function HomePage(): ReactElement {
   const navigation = useNavigation();
@@ -49,56 +25,23 @@ export default function HomePage(): ReactElement {
 
   console.log('data', data.length);
 
-  // const Handlegenerator = (props: any) => {
-  //   const {item, itemKey} = props;
-  //   const qrRef = useRef<any>();
-
-  //   const shareQRCodeImage = async () => {
-  //     try {
-  //       qrRef.current.capture().then((uri) => {
-  //         console.log(uri);
-  //         fetch(uri)
-  //           .then((response) => response.blob())
-  //           .then((blob) => {
-  //             //Upload imageFile lên Firebase tại đây
-  //             const imageRef = refStorage(storage, `images/${itemKey}`); // Sử dụng hàm ref từ firebaseconfig
-  //             uploadBytes(imageRef, blob).then(snapshot => {
-  //               console.log('uploadBytes');
-  //               getDownloadURL(snapshot.ref).then(url => {
-  //                 console.log(url);
-  //               });
-  //             });
+  const handlegenerator = () => {
+    console.log("handlegenerator");
+    
+    for(const key in data) {
+      const newObj = {
+        Age: data[key].Age,
+        Email: `${data[key].Age}@gdsc.com-check`,
+        ID: data[key].ID,
+        LinkQR: `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${data[key].ID}`,
+        Name: data[key].Name,
+        Valid: data[key].Valid,
+      };
   
-  //           })
-  
-  //       }).catch((error) => {
-  //         console.log('Error while capturing QR code', error);
-  //       });
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
-
-  //   const newObj = {
-  //     Age: item.Age,
-  //     Email: `${item.Age}@gdsc.com-check`,
-  //     ID: item.ID,
-  //     LinkQR: '123',
-  //     Name: item.Name,
-  //     Valid: item.Valid,
-  //   };
-
-  //   useEffect(() => {
-  //     shareQRCodeImage();
-  //   }, []);
-  //   // updateById(itemKey, newObj);
-
-  //   return (
-  //     <ViewShot ref={qrRef}>
-  //       <QRCode value={item.ID.toString()} />
-  //     </ViewShot>
-  //   );
-  // };
+      updateById( data[key].ID, newObj);
+    }
+    
+  };
 
   useEffect(() => {
     const sheetRef = ref(
@@ -112,10 +55,10 @@ export default function HomePage(): ReactElement {
   }, []);
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      {isStart &&
+      {/* {isStart &&
         data.map((item: any, index: number) => (
           <QRCodeGenerator key={index} item={item} itemKey={index} />
-        ))}
+        ))} */}
 
       <TouchableOpacity
         style={{
@@ -124,11 +67,12 @@ export default function HomePage(): ReactElement {
           borderRadius: 5,
           marginTop: 10,
         }}
-        // onPress={handlegenerator}
-        onPress={() => {
-          setIsStart(true);
-          console.log('ssss');
-        }}>
+        onPress={handlegenerator}
+        // onPress={() => {
+        //   setIsStart(true);
+        //   console.log('ssss');
+        // }}
+      >
         <Text style={{color: 'white', textAlign: 'center'}}>
           QRCodeGenerator
         </Text>
